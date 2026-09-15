6ea4bf2b54c3ef1b0e240fa4bf61b26f7d3a58c5
Rosalynee Sahoo <rosalyneesahoo@gmail.com>
2026-09-15 21:52:03 +0530

    Update and rename Tower of hanoi to Tower of hanoi.c

--- Tower of hanoi.c
+++ Tower of hanoi.c
@@ -2,10 +2,8 @@
 #include <stdlib.h>
 void toh(int n,char s,char d,char t)
 {
-
     if(n>1)
     {
-
         toh(n-1,s,t,d);
         printf("\n Move %d disc from %c to %c",n,s,d);
         toh(n-1,t,d,s);
